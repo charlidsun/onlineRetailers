ed374420e8423e636e5d7ab2e56b44d998176d9b
charlidsun <sunjingge@outlook.com>
2018-08-04 16:13:23 +0800

    初始化项目

---
+++ onlineRetailers/src/test/java/com/sun/onlineRetailers/OnlineRetailersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sun.onlineRetailers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineRetailersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
